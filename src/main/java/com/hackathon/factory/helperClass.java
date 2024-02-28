cef80f0e608a2ffadd79bbac75ff5c303a63ea0d
Kaustubh <kolekarkaustubh@gmail.com>
2024-02-28 17:56:03 +0530

    update

--- src/main/java/com/hackathon/factory/helperClass.java
+++ src/main/java/com/hackathon/factory/helperClass.java
@@ -2,15 +2,19 @@ package com.hackathon.factory;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Base64;
 import java.util.Properties;
- 
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
  
 public class helperClass {
  
@@ -20,26 +24,48 @@ static Properties p;
 	public  static WebDriver initializeBrowser() throws IOException
 	{
 		//code to initialize the browser
-		if(getProperties().getProperty("execution_env").equalsIgnoreCase("local"))
+		if(getProperties().getProperty("execution_env").equalsIgnoreCase("remote"))
 		{
-			switch(getProperties().getProperty("browser").toLowerCase()) 
-			{
-			case "chrome":
-		        driver=new ChromeDriver();					 // Creating a new instance of ChromeDriver
-		        break;
-		    case "edge":
-		    	driver=new EdgeDriver();					 // Creating a new instance of EdgeDriver
-		        break;
-		    case "firefox":
-		    	driver=new FirefoxDriver();                  // Creating a new instance of FirefoxDriver
-		    	break;
-		    default:
-		        System.out.println("No matching browser");
-		        driver=null;
-			}
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			//os
+			if (getProperties().getProperty("os").equalsIgnoreCase("windows")) {
+			    capabilities.setPlatform(Platform.WIN11);
+			} else if (getProperties().getProperty("os").equalsIgnoreCase("mac")) {
+			    capabilities.setPlatform(Platform.MAC);
+			} else {
+			    System.out.println("No matching OS..");
+			      }
+			//browser
+			switch (getProperties().getProperty("browser").toLowerCase()) {
+			    case "chrome":
+			        capabilities.setBrowserName("chrome");
+			        break;
+			    case "edge":
+			        capabilities.setBrowserName("MicrosoftEdge");
+			        break;
+			    default:
+			        System.out.println("No matching browser");
+			     }
+	        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
 		}
-		
+		else if(getProperties().getProperty("execution_env").equalsIgnoreCase("local"))
+			{
+				switch(getProperties().getProperty("browser").toLowerCase()) 
+				{
+				case "chrome":
+			        driver=new ChromeDriver();
+			        break;
+			    case "edge":
+			    	driver=new EdgeDriver();
+			        break;
+			    default:
+			        System.out.println("No matching browser");
+			        driver=null;
+				}
+			}
+		 driver.manage().deleteAllCookies(); 
 		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//		 driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
 		 return driver;					   //returning the driver
 	}
 	
